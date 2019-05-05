3ea62a2d24889e09efdb3113263db9bff1e9af1e
17110220 <43233472+17110220@users.noreply.github.com>
2019-05-05 22:48:55 +0700

    Tay

---
+++ QuanLySieuThi/QuanLySieuThi/DanhMucKhachHang.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace QuanLySieuThi
+{
+    partial class DanhMucKhachHang
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btndong = new System.Windows.Forms.Button();
+            this.btnboqua = new System.Windows.Forms.Button();
+            this.btnluu = new System.Windows.Forms.Button();
+            this.btnsua = new System.Windows.Forms.Button();
+            this.btnxoa = new System.Windows.Forms.Button();
+            this.btnthem = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lbdienthoai = new System.Windows.Forms.Label();
+            this.lbtenkh = new System.Windows.Forms.Label();
+            this.lbdiachi = new System.Windows.Forms.Label();
+            this.lbmakh = new System.Windows.Forms.Label();
+            this.lbdanhmucquanlykhachhang = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSize = true;
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.panel1.Controls.Add(this.btndong);
+            this.panel1.Controls.Add(this.btnboqua);
+            this.panel1.Controls.Add(this.btnluu);
+            this.panel1.Controls.Add(this.btnsua);
+            this.panel1.Controls.Add(this.btnxoa);
+            this.panel1.Controls.Add(this.btnthem);
+            this.panel1.Controls.Add(this.dataGridView1);
+            this.panel1.Controls.Add(this.textBox4);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.lbdienthoai);
+            this.panel1.Controls.Add(this.lbtenkh);
+            this.panel1.Controls.Add(this.lbdiachi);
+            this.panel1.Controls.Add(this.lbmakh);
+            this.panel1.Controls.Add(this.lbdanhmucquanlykhachhang);
+            this.panel1.Location = new System.Drawing.Point(12, 8);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(776, 435);
+            this.panel1.TabIndex = 2;
+            // 
+            // btndong
+            // 
+            this.btndong.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btndong.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btndong.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btndong.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btndong.Location = new System.Drawing.Point(689, 372);
+            this.btndong.Name = "btndong";
+            this.btndong.Size = new System.Drawing.Size(75, 45);
+            this.btndong.TabIndex = 20;
+            this.btndong.Text = "Đóng";
+            this.btndong.UseVisualStyleBackColor = false;
+            // 
+            // btnboqua
+            // 
+            this.btnboqua.BackColor = System.Drawing.Color.Olive;
+            this.btnboqua.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnboqua.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnboqua.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnboqua.Location = new System.Drawing.Point(557, 372);
+            this.btnboqua.Name = "btnboqua";
+            this.btnboqua.Size = new System.Drawing.Size(75, 45);
+            this.btnboqua.TabIndex = 19;
+            this.btnboqua.Text = "Bỏ qua";
+            this.btnboqua.UseVisualStyleBackColor = false;
+            // 
+            // btnluu
+            // 
+            this.btnluu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnluu.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnluu.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnluu.ForeColor = System.Drawing.Color.Blue;
+            this.btnluu.Location = new System.Drawing.Point(413, 372);
+            this.btnluu.Name = "btnluu";
+            this.btnluu.Size = new System.Drawing.Size(75, 45);
+            this.btnluu.TabIndex = 18;
+            this.btnluu.Text = "Lưu";
+            this.btnluu.UseVisualStyleBackColor = false;
+            // 
+            // btnsua
+            // 
+            this.btnsua.BackColor = System.Drawing.Color.Yellow;
+            this.btnsua.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnsua.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnsua.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnsua.Location = new System.Drawing.Point(280, 372);
+            this.btnsua.Name = "btnsua";
+            this.btnsua.Size = new System.Drawing.Size(75, 45);
+            this.btnsua.TabIndex = 17;
+            this.btnsua.Text = "Sửa";
+            this.btnsua.UseVisualStyleBackColor = false;
+            // 
+            // btnxoa
+            // 
+            this.btnxoa.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnxoa.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnxoa.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnxoa.ForeColor = System.Drawing.Color.Navy;
+            this.btnxoa.Location = new System.Drawing.Point(140, 372);
+            this.btnxoa.Name = "btnxoa";
+            this.btnxoa.Size = new System.Drawing.Size(75, 45);
+            this.btnxoa.TabIndex = 16;
+            this.btnxoa.Text = "Xóa";
+            this.btnxoa.UseVisualStyleBackColor = false;
+            // 
+            // btnthem
+            // 
+            this.btnthem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnthem.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnthem.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnthem.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.btnthem.Location = new System.Drawing.Point(14, 372);
+            this.btnthem.Name = "btnthem";
+            this.btnthem.Size = new System.Drawing.Size(75, 45);
+            this.btnthem.TabIndex = 15;
+            this.btnthem.Text = "Thêm";
+            this.btnthem.UseVisualStyleBackColor = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 128);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(752, 238);
+            this.dataGridView1.TabIndex = 14;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(498, 89);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(264, 20);
+            this.textBox4.TabIndex = 11;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(499, 53);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(264, 20);
+            this.textBox3.TabIndex = 10;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(139, 93);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(264, 20);
+            this.textBox2.TabIndex = 9;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(140, 56);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(264, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // lbdienthoai
+            // 
+            this.lbdienthoai.AutoSize = true;
+            this.lbdienthoai.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbdienthoai.Location = new System.Drawing.Point(409, 96);
+            this.lbdienthoai.Name = "lbdienthoai";
+            this.lbdienthoai.Size = new System.Drawing.Size(83, 16);
+            this.lbdienthoai.TabIndex = 4;
+            this.lbdienthoai.Text = "Điên Thoại";
+            // 
+            // lbtenkh
+            // 
+            this.lbtenkh.AutoSize = true;
+            this.lbtenkh.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbtenkh.Location = new System.Drawing.Point(11, 96);
+            this.lbtenkh.Name = "lbtenkh";
+            this.lbtenkh.Size = new System.Drawing.Size(121, 16);
+            this.lbtenkh.TabIndex = 3;
+            this.lbtenkh.Text = "Tên khách Hàng";
+            // 
+            // lbdiachi
+            // 
+            this.lbdiachi.AutoSize = true;
+            this.lbdiachi.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbdiachi.Location = new System.Drawing.Point(409, 56);
+            this.lbdiachi.Name = "lbdiachi";
+            this.lbdiachi.Size = new System.Drawing.Size(57, 16);
+            this.lbdiachi.TabIndex = 2;
+            this.lbdiachi.Text = "Địa Chỉ";
+            // 
+            // lbmakh
+            // 
+            this.lbmakh.AutoSize = true;
+            this.lbmakh.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbmakh.Location = new System.Drawing.Point(11, 56);
+            this.lbmakh.Name = "lbmakh";
+            this.lbmakh.Size = new System.Drawing.Size(116, 16);
+            this.lbmakh.TabIndex = 1;
+            this.lbmakh.Text = "Mã Khách Hàng";
+            // 
+            // lbdanhmucquanlykhachhang
+            // 
+            this.lbdanhmucquanlykhachhang.AutoSize = true;
+            this.lbdanhmucquanlykhachhang.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbdanhmucquanlykhachhang.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.lbdanhmucquanlykhachhang.Location = new System.Drawing.Point(245, 18);
+            this.lbdanhmucquanlykhachhang.Name = "lbdanhmucquanlykhachhang";
+            this.lbdanhmucquanlykhachhang.Size = new System.Drawing.Size(310, 24);
+            this.lbdanhmucquanlykhachhang.TabIndex = 0;
+            this.lbdanhmucquanlykhachhang.Text = "Danh Mục Quản Lý Khách Hàng";
+            // 
+            // DanhMucKhachHang
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::QuanLySieuThi.Properties.Resources.hinh_nen_powerpoint_don_gian_dep_2_045404959;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "DanhMucKhachHang";
+            this.Text = "Danh Mục Khách Hàng";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btndong;
+        private System.Windows.Forms.Button btnboqua;
+        private System.Windows.Forms.Button btnluu;
+        private System.Windows.Forms.Button btnsua;
+        private System.Windows.Forms.Button btnxoa;
+        private System.Windows.Forms.Button btnthem;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lbdienthoai;
+        private System.Windows.Forms.Label lbtenkh;
+        private System.Windows.Forms.Label lbdiachi;
+        private System.Windows.Forms.Label lbmakh;
+        private System.Windows.Forms.Label lbdanhmucquanlykhachhang;
+    }
+}
